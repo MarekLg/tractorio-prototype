cec05f9ba6cb1230b6bbc31b412ae8366dd0673b
Marek Legris <m.legris@freenet.de>
2026-03-01 14:24:26 +0100

    fix: networked player

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public sealed class CameraTarget : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        
+        if (IsLocalPlayer)
+        {
+            FindAnyObjectByType<CameraController>().SetTarget(transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Testing/MultiplayerTestingInit.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Multiplayer.PlayMode;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Testing
+{
+    [RequireComponent(typeof(NetworkManager))]
+    public class MultiplayerTestingInit : MonoBehaviour
+    {
+        private void Start()
+        {
+            var networkManager = GetComponent<NetworkManager>();
+
+            if (CurrentPlayer.IsMainEditor)
+            {
+                networkManager.StartHost();
+            }
+            else
+            {
+                networkManager.StartClient();
+            }
+        }
+    }
+}
